9bb1d4cb044fadaee62ffd6f2a8d3b8797dec4de
Corwin Crownover <corwin.crownover@gmail.com>
2018-01-19 16:18:16 +0800

    Extract CustomNavigationController class

---
+++ Companies/CustomNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationController.swift
+//  Companies
+//
+//  Created by Corwin Crownover on 1/19/18.
+//  Copyright © 2018 Corwin LLC. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
