89d7b66444bf0faa6d8f363ab2ef1d7927512731
Corwin Crownover <corwin.crownover@gmail.com>
2018-01-19 16:33:15 +0800

    Add name label

--- Companies/CreateCompanyController.swift
+++ Companies/CreateCompanyController.swift
@@ -9,13 +9,41 @@
 import UIKit
 
 class CreateCompanyController: UIViewController {
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.translatesAutoresizingMaskIntoConstraints = false // Use autolayout
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
         
         navigationItem.title = "Create Company"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
         
         view.backgroundColor = .darkBlue
+    }
+    
+    private func setupUI() {
+        let lightBlueBackgroundView = UIView()
+        lightBlueBackgroundView.backgroundColor = .lightBlue
+        lightBlueBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(lightBlueBackgroundView)
+        lightBlueBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        lightBlueBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lightBlueBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameLabel)
+        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        nameLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        nameLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
     }
     
     @objc func handleCancel() {
