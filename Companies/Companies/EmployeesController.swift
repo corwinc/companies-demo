da3b71fa3554bf47534d10ac63dfa9e253e18dd1
Corwin Crownover <corwin.crownover@gmail.com>
2018-01-20 19:08:30 +0800

    Add EmployeesController

---
+++ Companies/Companies/EmployeesController.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeesController.swift
+//  Companies
+//
+//  Created by Corwin Crownover on 1/20/18.
+//  Copyright © 2018 Corwin LLC. All rights reserved.
+//
+
+import UIKit
+
+class EmployeesController: UITableViewController {
+    var company: Company?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.title = company?.name
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .yellow
+    }
+}
